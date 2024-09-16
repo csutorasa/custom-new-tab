64fac82ad492b1b0afe0bfaf6e67e4211f6f16dd
Ármin Csutorás <csutorasa@users.noreply.github.com>
2024-09-16 23:16:28 +0200

    Update pack.sh description

--- pack.sh
+++ pack.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# dependencies:
+# - zip
+# - dirname
+# - rm
 cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null
 rm -f release.zip
 zip release.zip index.html index.js manifest.json options.html options.js storage.js translations.js
